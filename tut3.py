a32c19ed1a7e855bce74195ac24683b63c44d2e3
kalilinux2009 <114347558+kalilinux2009@users.noreply.github.com>
2022-10-04 13:50:07 +0530

    Add files via upload

---
+++ tut3.py
@@ -0,0 +1,31 @@
+from ast import While
+from http.client import OK
+from time import sleep
+from re import A, I
+from tkinter import W
+import pygame
+
+attack = input('DOW YOU WANT TO START THE DOSS ATTACK (YES/NO) :')
+if attack == 'YES':
+    while True:
+     pygame.init()
+window = pygame.display.set_mode ((0,0) ,pygame.FULLSCREEN)
+sleep(300)
+i = 1
+while True:
+        print(i)
+        i = i + 1
+                
+
+    
+
+
+
+
+
+
+
+
+
+
+
